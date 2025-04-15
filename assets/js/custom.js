60c4e292be24a2373487633cebe4c4b257556e82
HeenaTechuz123 <heena@techuz.com>
2025-04-15 22:13:10 +0530

    responsive design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -161,24 +161,16 @@ const swiper = new Swiper('.swiper-contact', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-    // on: {
-    //   slideChange: function () {
-    //     const totalSlides = this.slides.length - this.loopedSlides * 2; 
-    //     const currentIndex = this.realIndex + 1;
-    //     const progress = (currentIndex / totalSlides) * 100;
-    //     document.querySelector('.progress-fill').style.width = progress + '%';
-    //   }
-    // }
+    },  
+     scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
     
    
   });
- // Set initial progress
-    window.addEventListener('load', () => {
-      document.querySelector('.progress-fill').style.width = '33.33%'; // for 3 slides
-    });
-
- $('#mySelect').select2({
+  
+  $('#mySelect').select2({
     placeholder: "Subject",
     allowClear: true
 });
