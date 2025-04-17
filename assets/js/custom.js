a949e07acc543b9c570732e881f1bfc6acc9c7c4
HeenaTechuz123 <heena@techuz.com>
2025-04-17 15:55:10 +0530

    solution page design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -348,3 +348,16 @@ $('.read-less').hide();
 
     $('.read-more-btn, .read-less').toggle();
   });
+
+
+  $(function() {
+	// (Optional) Active an item if it has the class "is-active"	
+	$(".accordion > .accordion-item.is-active").children(".accordion-panel").slideDown();
+	
+	$(".accordion > .accordion-item").click(function() {
+		// Cancel the siblings
+		$(this).siblings(".accordion-item").removeClass("is-active").children(".accordion-panel").slideUp();
+		// Toggle the item
+		$(this).toggleClass("is-active").children(".accordion-panel").slideToggle("ease-out");
+	});
+});
